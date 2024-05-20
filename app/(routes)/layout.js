756e23803e13c04a10b22a58cf99570418251a8f
Jaskaran1519 <jaskaran1519@gmail.com>
2024-05-20 13:08:36 +0530

    oopo

--- app/(routes)/layout.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const homelayout = ({ children }) => {
-  return (
-    <div className="overflow-hidden bg-white ">
-      <div className="  z-0">{children}</div>
-    </div>
-  );
-};
